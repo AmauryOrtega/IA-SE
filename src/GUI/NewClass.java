789e48e221d6c57c27b4abd7677c04dccf20094f
Amaury Ortega <amauryocortega@gmail.com>
2017-05-09 18:01:23 -0500

    falta prolog

--- src/GUI/NewClass.java
+++ src/GUI/NewClass.java
@@ -1,18 +1,17 @@
 package GUI;
 //PROLOG 1. Importando de jpl.jar
-import org.jpl7.*;
+
+import org.jpl7.Query;
 
 public class NewClass {
-    
-    public static void main (String[] agrs){
+
+    public static void main(String[] agrs) {
         //PROLOG 2. Conexion con el archivo prolog.pl
-        try{
-            String conexion = "consult('programa.pl')";
-            Query con = new Query(conexion);
-            System.out.println(conexion+"  "+(con.hasMoreSolutions()?"Aceptado":"Fallo"));
-        }catch (Exception e){
-            System.out.println("Error en cargando programa.pl\n"+e.getMessage()+"\n");
-        }
+
+        String consulta = "consult('programa.pl')";
+        Query qConsulta = new Query(consulta);
+//        System.out.println(qConsulta.hasSolution());
+
     }
-    
+
 }
